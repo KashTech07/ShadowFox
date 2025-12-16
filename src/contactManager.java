f3307c30cb66c324abb5a7f1581f228a8868cd7f
Kashish Sharma <kashishsuneetasharma@gmail.com>
2025-12-16 22:25:52 +0530

    created basic structure of contactManager class.

--- src/contactManager.java
+++ src/contactManager.java
@@ -6,7 +6,40 @@ import java.util.ArrayList;
 public class contactManager {
     public static Scanner sc = new Scanner(System.in);
     public static ArrayList<contact> list = new ArrayList<>();
-    public static void display(String[] args) {
+    public static void main(String[] args){
+        boolean running = true ;
+        while(running){
+            display();
+            if(!sc.hasNextInt()){
+                System.out.println("invalid input");
+                sc.nextLine();
+                continue;
+            }
+            int choice = sc.nextInt();
+            if(choice==1){
+                addContact();
+            }
+            else if(choice==2){
+                displayContact();
+            }
+            else if(choice==3){
+                updateContact();
+        }
+            else if(choice==4){
+                deleteContact();
+            }
+            else if(choice==5){
+                exit();
+            }
+    }}
+          static void addContact(){
+              contact c1 = new contact();
+              System.out.println("Name : ");
+              c1.name=
+
+          }
+
+    public static void display() {
         System.out.println("----\uD83D\uDCDEContact Manager System----");
         System.out.println("1. Add New Contact");
         System.out.println("2. View All Contacts");
