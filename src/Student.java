6af103bd2839af45ab46149365f6e1855069a423
Kashish Sharma <kashishsuneetasharma@gmail.com>
2025-12-18 15:46:57 +0530

    Initial Commit - Student Information system

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String id,name, gpa;
+
+    public Student(String id, String name, String gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+}
+
